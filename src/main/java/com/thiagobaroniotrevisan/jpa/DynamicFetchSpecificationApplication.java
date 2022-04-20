e7f64fad7eae3ba40143adc7ba7aba6553943c4a
Thiago Trevisan <thibtrevisan@gmail.com>
2022-04-19 20:29:24 -0700

    first commit

---
+++ src/main/java/com/thiagobaroniotrevisan/jpa/DynamicFetchSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.thiagobaroniotrevisan.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicFetchSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicFetchSpecificationApplication.class, args);
+	}
+
+}
